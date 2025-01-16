91b22637f7683842185618cf1b6b781095083ba3
Vigitoreanec <chirik39@mail.ru>
2025-01-16 14:33:22 +0200

    удалили лишнее

--- app.php
+++ app.php
@@ -1,16 +1,6 @@
 <?php
 
-
-// require_once 'src/blog.php';
-// require_once 'src/helpers.php';
-// require_once 'src/main.php';
-// require_once 'src/db.php';
-
-print_r(PDO::getAvailableDrivers());
-
 require __DIR__  . "/vendor/autoload.php";
-
-
 
 try{
     $rezult = main();
